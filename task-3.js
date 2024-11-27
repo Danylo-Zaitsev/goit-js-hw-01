1bcb2d2ed58eb7c7b1ec585ba818821291ae406f
Danylo-Zaitsev <danilverehko@gmail.com>
2024-11-27 18:58:42 +0100

    zminy

--- task-3.js
+++ task-3.js
@@ -1,10 +1,10 @@
 function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2; // Падінг з двох сторін
-  const borderWidth = parseFloat(border) * 2; // Бордер з двох сторін
+  const paddingWidth = parseFloat(padding) * 2;
+  const borderWidth = parseFloat(border) * 2;
   return contentWidth + paddingWidth + borderWidth;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
